d80c6d6062550915f00bc419799fa5dd01d7ff5b
marikna <147309862+marikna@users.noreply.github.com>
2025-10-11 16:11:11 +0200

    sum of salaries

---
+++ 01_setup/zad5m.sql
@@ -0,0 +1,6 @@
+SELECT (SUM(pe.kwota) + SUM(pr.kwota)) AS suma_wynagrodzen
+FROM ksiegowosc.pensja pe
+JOIN ksiegowosc.wynagrodzenie w
+ON pe.id_pensji = w.id_pensji
+JOIN ksiegowosc.premia pr
+ON w.id_premii = pr.id_premii
